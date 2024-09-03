761107e253e73d1212c972fafa0e31ebfb72ea53
Edwin Mantilla Santamaria <emantilla@deloitte.com>
2024-09-02 20:14:40 -0500

    Playwright Class 3

---
+++ Class3/tests/home.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Automation Exercise', ()=>{
+    test('Register user', async ({ page }) => {
+        //Dirigirse a la pagina
+        await page.goto('https://automationexercise.com/')
+        //Validar que la pagina se cargue correctamente
+        await expect(page.getByAltText('Website for automation practice')).toBeVisible()
+        //Click en el link de signup
+        await page.getByRole('link', { name: 'Signup / Login' }).click()
+        //Validar que se cargue la pagina de signup
+        await expect(page.getByText('New User Signup!')).toBeVisible()
+        //Escribir datos en los campos
+        await page.getByPlaceholder('Name').fill('Hackpro')
+        await page.locator('[data-qa="signup-email"]').fill('hackpro@yopmail.com')
+        //Click en boton signup
+        await page.getByRole('button', { name: 'Signup' }).click()
+        //Click en MR. o MRs.
+        await page.getByRole('radio', { name: 'Mr.' }).click()
+        //Rellenar password
+        await page.locator('#password').fill('hackprotm')
+        //Seleccionar fecha de nacimiento
+        await page.locator('#days').selectOption('7')
+        await page.locator('#months').selectOption('October')
+        await page.locator('#years').selectOption('1981')
+        //Otra opcion  de rellenar los dias
+        //const days = page.locator('#days')
+        //await days.selectOption('14')
+        await page.locator('#newsletter').check()
+        await page.locator('#first_name').fill('Hackpro')
+        await page.locator('#last_name').fill('Team')
+        await page.locator('#company').fill('Arroyo Consulting')
+        await page.locator('#address1').fill('Fake St. 123')
+        await page.locator('#country').selectOption('India')
+        await page.getByRole('textbox', { name: 'State *' }).fill('Delhi')
+        await page.getByRole('textbox', { name: 'City *' }).fill('Delhi')
+        await page.locator('#zipcode').fill('1234')
+        await page.locator('#mobile_number').fill('999999999')
+        await page.getByRole('button', { name: 'Create Account' }).click()
+        await expect(page.getByText('Account Created!')).toBeVisible()
+        await page.getByRole('link', { name: 'Continue' }).click()
+    })
+})
